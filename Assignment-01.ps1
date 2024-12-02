3f7b0c0861c0790f7baaa8d5f4ac9e255ecc7b1e
MOMOROPC\acost <acosta.juan@icloud.com>
2024-12-01 20:14:06 -0500

    Added the Switch for choosing between functions and to exit the Do Loop

--- Assignment-01.ps1
+++ Assignment-01.ps1
@@ -15,6 +15,23 @@ Do {
 
 
 
+    Write-Host "Select a function to execute:"
+    Write-Host "1 for CreateCSV"
+    Write-Host "2 for ModifyCSV"
+    Write-Host "3 for ImportFromCSV"
+    Write-Host "4 for Exit"
+    $Choice = (Read-Host -Prompt "Enter the number of your choice")
+        if ([string]::IsNullOrWhiteSpace($Choice)) {
+            Write-Host "A function has not been selected" -ForegroundColor Yellow
+        }
+        Switch ($Choice) {
+            1 { CreateCSV }
+            2 { ModifyCSV }
+            3 { ImportFromCSV }
+            4 { ForExit }
+            Default { Write-Host "Invalid choice. Please try again." -ForegroundColor Yellow }
+        }  
+
     } while ($Global:Exit -ne "y")
 
 Write-Host "Program exited. Goodbye!" -ForegroundColor Green -BackgroundColor Yellow  
