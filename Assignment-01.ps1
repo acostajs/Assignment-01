01ae16009644d1733aeabfd9bbaf3ac23515eeaf
MOMOROPC\acost <acosta.juan@icloud.com>
2024-12-01 20:42:40 -0500

    Added a Function to Create a CSV file and be able to create content and read the content from the newly created CSV file.

--- Assignment-01.ps1
+++ Assignment-01.ps1
@@ -11,6 +11,94 @@ function ForExit {
 }
 
 Do {
+    
+    function CreateCSV {
+        Write-Host "You have choosen to Create an CSV"
+        $Path = (Read-Host -Prompt "Type the direction where you want to create a new folder")
+            if ([string]::IsNullOrWhiteSpace($Path)) {
+                Write-Host "You have not written anything, exiting ..."
+            return
+            }
+            else {
+                if (-Not (Test-Path -Path $Path)) {
+                    Write-Host "The path '$Path' does not exist. Please enter a valid directory." -ForegroundColor Red
+                    return
+                } 
+                else {
+                    $FolderName = (Read-Host -Prompt "Type the name of your new folder")
+                        if ([string]::IsNullOrWhiteSpace($FolderName)) {
+                            Write-Host "You have not written anything, exiting ..."
+                            exit
+                        }
+                        elseif (Test-Path -Path "$Path\$FolderName"){
+                            Write-Host "The folder at your choosen direction already exist." -ForegroundColor Yellow
+                        }
+                        else {
+                        mkdir "$Path\$FolderName"
+                        Write-Host "Your new folder has been created in $path\$FolderName as you can see above"
+                        }
+                }
+            }    
+        $NewPath = "$Path\$FolderName"
+        $FileName = (Read-Host -Prompt "How are you going to name your new file?")
+        if ([string]::IsNullOrWhiteSpace($FileName)) {
+            Write-Host "You have not written anything"
+            return
+        }
+        $File = "$FileName.csv"
+        $FullPath = Join-Path -Path $NewPath -ChildPath $File
+        
+
+            if(Test-Path -Path $FullPath) {
+                Write-Host "The file $FullPath already exists." -ForegroundColor Yellow
+                return
+            }
+            else {
+                New-Item -Path $NewPath -Name $File -ItemType "File"
+                Write-Host "Your new file has been created at $FullPath"
+            }
+        Write-Host "Add some content to your new CSV file"
+        $Global:FullPath = $FullPath
+        Add-Content -Path $FullPath
+    
+        do {
+            $Read = (Read-Host -Prompt "Would you like to read what you just wrote? (y/n)")
+                if ([string]::IsNullOrWhiteSpace($Read)) {
+                    Write-Host "Please input a valid answer" -ForegroundColor Yellow
+                    return
+                }   
+                elseif($Read.ToLower() -eq "n") {
+                    Write-Host "You chose not to read the file."
+                }
+                elseif($Read.ToLower() -eq "y") {
+                    Write-Host "Here is the content you wrote:"
+                    Import-CSV $FullPath
+                }
+                else {
+                    Write-Host "Invalid input. Please input 'y' for yes or 'n' for no." -ForegroundColor Yellow
+                }
+
+            $WriteAgain = (Read-Host -Prompt "Would you like to add more content to $File? (y/n)")
+                if ([string]::IsNullOrWhiteSpace($WriteAgain)) {
+                    Write-Host "Please input a valid answer" -ForegroundColor Yellow
+                    return
+                }   
+                elseif($WriteAgain.ToLower() -eq "n") {
+                    Write-Host "You chose not to add more content."
+                }
+                elseif($WriteAgain.ToLower() -eq "y") {
+                    Write-Host "Add some content to your new CSV file"
+                    Add-Content -Path $FullPath
+                }
+                else {
+                    Write-Host "Invalid input. Please input 'y' for yes or 'n' for no." -ForegroundColor Yellow
+                }
+
+        } while ($Read.ToLower() -ne "n" -and $WriteAgain.ToLower() -ne "n")
+        return $FullPath
+    } 
+
+    
 
 
 
